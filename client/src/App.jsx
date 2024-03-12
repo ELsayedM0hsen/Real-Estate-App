0584fdc15e1bcd732b53ad98e0ec0ff3f0f7edbf
ELsayedM0hsen <elsayedmawad01@gmail.com>
2024-03-12 02:01:43 +0200

    home-v1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import "./layout.scss";
 import Navbar from "./components/navbar/Navbar";
-import Footer from "./components/footer/Footer";
-
+// import Footer from "./components/footer/Footer";
+import Home from "./pages/Home/Home";
 
 const App = () => {
-  return <div className="layout">
-    <Navbar/>
-    <br/>
-    {/* <Footer/> */}
-  </div>;
+  return (
+    <div className="layout">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="content">
+        <Home />
+      </div>
+    </div>
+  );
 };
-
 
 export default App;
--- client/src/components/searchBar/SearchBar.jsx
+++ client/src/components/searchBar/SearchBar.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React, { useState } from "react";
+import search from "../../../public/search.png";
+import "./searchBar.scss";
+
+const types = ["buy", "rent"];
 
 const SearchBar = () => {
-  return (
-    <div>SearchBar</div>
-  )
-}
+  const [query, setQuery] = useState({
+    type: "buy",
+    location: "",
+    minPrice: 0,
+    maxPrice: 0,
+  });
 
-export default SearchBar
+  const switchType = (val) => {
+    setQuery((prev) => ({ ...prev, type: val }));
+  };
+
+  return (
+    <div className="searchBar">
+      <div className="type">
+        {types.map((type) => (
+          <button key={type} className={query.type === type ? "active" : ""} 
+          onClick={()=>switchType(type)}
+          >
+            {type}
+          </button>
+        ))}
+      </div>
+      <form action="">
+        <input type="text" name="location" placeholder="City Location" />
+        <input
+          type="number"
+          name="minPrice"
+          min={0}
+          max={10000000}
+          placeholder="Min Price"
+        />
+        <input
+          type="number"
+          name="maxPrice"
+          min={0}
+          max={10000000}
+          placeholder="Max Price"
+        />
+        <button>
+          <img src={search} alt="search" />
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchBar;
